162564bbad056f081b7bda7859d3fe2d49b4146d
damilare <adesojidiekola@gmail.com>
2021-01-27 19:09:46 -0500

    pieces ready for authentification

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports= {
+    MONGODB: "mongodb+srv://classed:U0cGhsdE6pu7Q7Kg@cluster0.fv9k3.mongodb.net/mong?retryWrites=true&w=majority",
+    SECRET_KEY: "some very secret key"
+}
+
---
+++ graphql/resolvers/index.js
@@ -0,0 +1,12 @@
+const { model } = require('mongoose')
+const postsResolvers = require('./posts')
+const usersResolvers = require('./users')
+
+module.exports = {
+    Query: {
+        ...postsResolvers.Query
+    },
+    Mutation: {
+        ...usersResolvers.Mutation
+    }
+}
